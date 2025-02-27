5315d43027d8d438da7ddeea637faf7ace04f8db
driemworks <driemworks@idealabs.network>
2025-02-26 18:35:50 -0600

    update deps, add gossipsub to sealing logic wip

--- client/consensus/randomness-beacon/src/instant_seal.rs
+++ client/consensus/randomness-beacon/src/instant_seal.rs
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-//! # Inherent-Data Sync based consensus mechanism 
+//! # Inherent-Data Sync based consensus mechanism
 //!
-//! This consensus mechanism produces blocks whenever it has the necessary data as required by inherent data providers. 
+//! This consensus mechanism produces blocks whenever it has the necessary data as required by inherent data providers.
 //! Specifically, the goal is to author a block whenever there are N new messages in a queue
 //!
 
@@ -31,11 +31,10 @@
 // 	block_import::{BlockImport, BlockImportParams, ForkChoiceStrategy},
 // 	import_queue::{BasicQueue, BoxBlockImport, Verifier},
 // };
+use crate::gossipsub::{GossipsubNetwork, GossipsubState, SharedState};
+use libp2p::{gossipsub::Config as GossipsubConfig, identity::Keypair};
 use sc_consensus_manual_seal::{
-	consensus::ConsensusDataProvider, 
-	EngineCommand,
-	ManualSealParams,
-	run_manual_seal
+	consensus::ConsensusDataProvider, run_manual_seal, EngineCommand, ManualSealParams,
 };
 use sp_api::ProvideRuntimeApi;
 use sp_blockchain::HeaderBackend;
@@ -43,7 +42,11 @@ use sp_blockchain::HeaderBackend;
 // use sp_core::traits::SpawnNamed;
 use sp_inherents::CreateInherentDataProviders;
 use sp_runtime::{traits::Block as BlockT, ConsensusEngineId};
-use std::{marker::PhantomData, sync::Arc, time::Duration};
+use std::{
+	marker::PhantomData,
+	sync::{Arc, Mutex},
+	time::Duration,
+};
 
 const LOG_TARGET: &str = "drand-lock-step";
 
@@ -73,7 +76,6 @@ pub struct ConsensusParams<B: BlockT, BI, E, C: ProvideRuntimeApi<B>, TP, SC, CI
 	/// Something that can create the inherent data providers.
 	pub create_inherent_data_providers: CIDP,
 }
-
 
 // /// runs the background authorship task for the randomness-beacon pulse-based seal engine.
 // /// It creates a new block for every N pulses from a randomness beacon ingested into a queue
@@ -124,6 +126,7 @@ pub struct ConsensusParams<B: BlockT, BI, E, C: ProvideRuntimeApi<B>, TP, SC, CI
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use futures::StreamExt;
 	use sc_basic_authorship::ProposerFactory;
 	use sc_consensus::{BlockImportParams, ImportedAux};
 	use sc_consensus_manual_seal::{CreatedBlock, Error};
@@ -131,12 +134,11 @@ mod tests {
 	use sc_transaction_pool_api::{MaintainedTransactionPool, TransactionPool, TransactionSource};
 	use sp_inherents::InherentData;
 	use sp_runtime::generic::{Digest, DigestItem};
+	use substrate_test_runtime_client::AccountKeyring::Alice;
 	use substrate_test_runtime_client::{
 		DefaultTestClientBuilderExt, TestClientBuilder, TestClientBuilderExt,
 	};
-	use substrate_test_runtime_client::AccountKeyring::Alice;
 	use substrate_test_runtime_transaction_pool::{uxt, TestApi};
-	use futures::StreamExt;
 
 	fn api() -> Arc<TestApi> {
 		Arc::new(TestApi::empty())
@@ -174,6 +176,34 @@ mod tests {
 		}
 	}
 
+	async fn run_gossipsub() -> SharedState {
+		let topic_str: &str =
+			"/drand/pubsub/v0.0.0/52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971";
+
+		let maddr1: libp2p::Multiaddr =
+			"/ip4/184.72.27.233/tcp/44544/p2p/12D3KooWBhAkxEn3XE7QanogjGrhyKBMC5GeM3JUTqz54HqS6VHG"
+				.parse()
+				.expect("The string is a well-formatted multiaddress. qed.");
+
+		let maddr2: libp2p::Multiaddr =
+        "/ip4/54.193.191.250/tcp/44544/p2p/12D3KooWQqDi3D3KLfDjWATQUUE4o5aSshwBFi9JM36wqEPMPD5y"
+            .parse()
+            .expect("The string is a well-formatted multiaddress. qed.");
+
+		let local_identity: Keypair = Keypair::generate_ed25519();
+		let state = Arc::new(Mutex::new(GossipsubState { pulses: vec![] }));
+		let gossipsub_config = GossipsubConfig::default();
+		let mut gossipsub =
+			GossipsubNetwork::new(&local_identity, state.clone(), gossipsub_config).unwrap();
+
+		tokio::spawn(async move {
+			if let Err(e) = gossipsub.run(topic_str, vec![&maddr1, &maddr2], None).await {
+				log::error!("Failed to run gossipsub network: {:?}", e);
+			}
+		});
+
+		state
+	}
 
 	#[tokio::test]
 	async fn instant_seal() {
@@ -194,11 +224,18 @@ mod tests {
 			genesis_hash,
 			genesis_hash,
 		));
+
 		let env = ProposerFactory::new(spawner.clone(), client.clone(), pool.clone(), None, None);
 		// this test checks that blocks are created as soon as transactions are imported into the
 		// pool.
 		let (sender, receiver) = futures::channel::oneshot::channel();
 		let mut sender = Arc::new(Some(sender));
+
+		// so we want to redefine the commands stream
+		let mut shared_state = run_gossipsub().await;
+		// shared_state. 
+
+
 		let commands_stream =
 			pool.pool().validated_pool().import_notification_stream().map(move |_| {
 				// we're only going to submit one tx so this fn will only be called once.
@@ -248,5 +285,4 @@ mod tests {
 		assert!(client.header(created_block.hash).unwrap().is_some());
 		assert_eq!(client.header(created_block.hash).unwrap().unwrap().number, 1)
 	}
-
 }
