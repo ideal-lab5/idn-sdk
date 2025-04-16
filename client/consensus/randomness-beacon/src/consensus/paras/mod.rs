8ffb504e41796e44d2c89ee62ac235f6d1a13521
driemworks <driemworks@idealabs.network>
2025-04-16 15:55:06 -0500

    integrate last round notifier to para consensus

--- client/consensus/randomness-beacon/src/consensus/paras/mod.rs
+++ client/consensus/randomness-beacon/src/consensus/paras/mod.rs
@@ -13,9 +13,5 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-mod solochain;
-pub use solochain::manual_seal;
-
-mod paras;
-pub use paras::aura;
+ 
+pub mod aura;
