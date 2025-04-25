b19b3972b892f2a1100cbaf4eaf7a3214bee7dfb
Juan Girini <juangirini@gmail.com>
2025-04-25 09:19:11 -0300

    wip

--- chains/ideal-network/bridge/src/lib.rs
+++ chains/ideal-network/bridge/src/lib.rs
@@ -21,7 +21,7 @@
 pub mod types;
 
 use frame_support::pallet_prelude::{Decode, Encode, TypeInfo};
-use types::CreateSubParams;
+use types::{CreateSubParams, SubscriptionId};
 
 /// A minimized version of `pallet-idn-manager::Call` that can be used without a runtime.
 #[derive(Encode, Decode, Debug, PartialEq, Clone, TypeInfo)]
@@ -30,6 +30,18 @@ pub enum IdnManagerCall {
 	/// `pallet-idn-manager::Call::create_subscription`
 	#[codec(index = 0)]
 	create_subscription { params: CreateSubParams },
+	/// `pallet-idn-manager::Call::pause_subscription`
+	#[codec(index = 1)]
+	pause_subscription { sub_id: SubscriptionId },
+	/// `pallet-idn-manager::Call::kill_subscription`
+	#[codec(index = 2)]
+	kill_subscription { sub_id: SubscriptionId },
+	/// `pallet-idn-manager::Call::update_subscription`
+	#[codec(index = 3)]
+	update_subscription { params: UpdateSubParams },
+	/// `pallet-idn-manager::Call::reactivate_subscription`
+	#[codec(index = 4)]
+	reactivate_subscription { sub_id: SubscriptionId },
 }
 
 /// `Idn` Runtime `Call` enum.
