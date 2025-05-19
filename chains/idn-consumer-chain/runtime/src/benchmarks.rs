e5e8ba9b47047c4a00b41ec34736022edf8b0d18
Juan Girini <juangirini@gmail.com>
2025-05-19 22:07:35 +0200

    add revive weights

--- chains/idn-consumer-chain/runtime/src/benchmarks.rs
+++ chains/idn-consumer-chain/runtime/src/benchmarks.rs
@@ -27,5 +27,7 @@ frame_benchmarking::define_benchmarks!(
 	[pallet_message_queue, MessageQueue]
 	[pallet_transaction_payment, TransactionPayment]
 	[pallet_idn_consumer, IdnConsumer]
-	[pallet_xcm, PolkadotXcm]
+	// [pallet_contracts, Contracts]
+	[pallet_revive, Revive]
+	// [pallet_xcm, PolkadotXcm]
 );
