b71ac8c53ad8c14a77b9362d575ea91b98ab6337
Juan Girini <juangirini@gmail.com>
2025-05-20 17:33:12 +0200

    clean up

--- chains/idn-consumer-chain/runtime/src/benchmarks.rs
+++ chains/idn-consumer-chain/runtime/src/benchmarks.rs
@@ -27,7 +27,6 @@ frame_benchmarking::define_benchmarks!(
 	[pallet_message_queue, MessageQueue]
 	[pallet_transaction_payment, TransactionPayment]
 	[pallet_idn_consumer, IdnConsumer]
-	// [pallet_contracts, Contracts]
+	[pallet_contracts, Contracts]
 	[pallet_revive, Revive]
-	// [pallet_xcm, PolkadotXcm]
 );
