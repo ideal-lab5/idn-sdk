7b688d1fadbbefa4bec05b2061e0d9e8736248eb
Tony Riemer <driemworks@idealabs.network>
2025-07-28 04:39:09 -0500

    Fix/pulse-drain (#294)
    * add tests pulse drain, refactor
    
    * fmt
    
    * bump runtime version
    
    ---------
    
    Co-authored-by: Juan Girini <juangirini@gmail.com>

--- chains/ideal-network/runtime/src/lib.rs
+++ chains/ideal-network/runtime/src/lib.rs
@@ -166,7 +166,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
 	spec_name: Cow::Borrowed("idn-runtime"),
 	impl_name: Cow::Borrowed("idn-runtime"),
 	authoring_version: 1,
-	spec_version: 2,
+	spec_version: 3,
 	impl_version: 0,
 	apis: apis::RUNTIME_API_VERSIONS,
 	transaction_version: 1,
