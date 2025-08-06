78de08cb95faaf474575274175b60e698ceb3fa5
Coleman Irby <ckirbyee92@gmail.com>
2025-08-06 12:09:54 -0500

    Cki/e2e xcm (#307)
    * working version with HRMP channels opened
    
    * zombienet now properly using IDN and IDN Consumer chains
    
    * Tests now set randBeacon config. Using proper parachain collator nodes.
    
    * adding script for adding balance to treasury and sovereign account
    
    * remove incomplete code
    
    * Quote Subscription test implemented
    
    * Initial e2e smoketests completed for Subscription logic
    
    * update README
    
    * db verification completed
    
    * db verificaitons complete, added new verifications that randomness is received when subscription is updated
    
    * added log statement when randomness is dispatched
    
    * removed unused chainspec

--- pallets/idn-manager/src/lib.rs
+++ pallets/idn-manager/src/lib.rs
@@ -879,6 +879,7 @@ impl<T: Config> Pallet<T> {
 					continue;
 				}
 
+				log::info!(target: LOG_TARGET, "Randomness distributed");
 				Self::deposit_event(Event::RandomnessDistributed { sub_id });
 			} else {
 				let idle_credits = T::FeesManager::get_idle_credits(Some(&sub));
