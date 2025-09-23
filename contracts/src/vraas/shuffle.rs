7cfe86415b04271fbff8b5277bcdfd5d1dacaa6e
driemworks <driemworks@idealabs.network>
2025-09-23 13:40:13 -0500

    remove unused import

--- contracts/src/vraas/shuffle.rs
+++ contracts/src/vraas/shuffle.rs
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 use crate::ext::{IDNEnvironment, RandomReadErr};
-use alloc::vec::Vec;
 use ink::EnvAccess;
 use rand::{seq::SliceRandom, SeedableRng};
 use rand_chacha::ChaCha12Rng;
