44308a3e78d9490a846a0a8536d702ff86a52855
driemworks <driemworks@idealabs.network>
2025-09-23 16:38:44 -0500

    precomp wip

--- precompiles/src/bls12.rs
+++ precompiles/src/bls12.rs
@@ -6,5 +6,33 @@ use pallet_revive::{
 		alloy::{self, sol_types::SolValue},
 		AddressMatcher, Error, Ext, Precompile,
 	},
-	DispatchInfo, Origin,
+	Config, DispatchInfo, Origin,
 };
+
+/// A Bls12-381 precompile.
+pub struct Bls12_381<Runtime, PrecompileConfig, Instance = ()> {
+	_phantom: PhantomData<(Runtime, PrecompileConfig, Instance)>,
+}
+
+impl<Runtime, PrecompileConfig, Instance: 'static> Precompile
+	for Bls12_381<Runtime, PrecompileConfig, Instance>
+where
+	Runtime: pallet_revive::Config,
+
+	// // Note can't use From as it's not implemented for alloy::primitives::U256 for unsigned types
+	// alloy::primitives::U256: TryFrom<<Runtime as Config<Instance>>::Balance>,
+{
+	type T = Runtime;
+	type Interface = ();//IERC20::IERC20Calls;
+	const MATCHER: AddressMatcher = AddressMatcher::Fixed(NonZero::new(10).unwrap());
+	const HAS_CONTRACT_INFO: bool = false;
+
+	fn call(
+		address: &[u8; 20],
+		input: &Self::Interface,
+		env: &mut impl Ext<T = Self::T>,
+	) -> Result<Vec<u8>, Error> {
+        let out = vec![];
+		Ok(out)
+	}
+}
