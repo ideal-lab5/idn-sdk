7c4209ce38514860cb268033ad4758fa771ed9cf
driemworks <driemworks@idealabs.network>
2025-10-16 11:25:57 -0500

    validate unsigned test for invalidating old start rounds

--- pallets/randomness-beacon/src/tests.rs
+++ pallets/randomness-beacon/src/tests.rs
@@ -17,11 +17,11 @@
 use crate::{
 	mock::{ALICE, *},
 	weights::WeightInfo,
-	BeaconConfig, DidUpdate, Error, LatestRound, SparseAccumulation,
+	BeaconConfig, DidUpdate, Error, NextRound, SparseAccumulation,
 };
 use frame_support::{assert_noop, assert_ok, traits::OnFinalize};
-use sp_idn_crypto::test_utils::{get, get_beacon_pk, PULSE1000, PULSE1001, PULSE1002, PULSE1003};
 use sp_core::Pair;
+use sp_idn_crypto::test_utils::{get, get_beacon_pk, PULSE1000, PULSE1001, PULSE1002, PULSE1003};
 
 #[test]
 fn can_construct_pallet_and_set_genesis_params() {
@@ -29,8 +29,8 @@ fn can_construct_pallet_and_set_genesis_params() {
 		let actual_initial_sigs = SparseAccumulation::<Test>::get();
 		assert!(actual_initial_sigs.is_none());
 
-		let latest_round = LatestRound::<Test>::get();
-		assert_eq!(latest_round, 0);
+		let next_round = NextRound::<Test>::get();
+		assert_eq!(next_round, 0);
 	});
 }
 
@@ -46,7 +46,13 @@ fn can_fail_write_pulse_when_beacon_config_not_set() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 		assert_noop!(
-			Drand::try_submit_asig(RuntimeOrigin::none(), asig.try_into().unwrap(), start, end, signature.into()),
+			Drand::try_submit_asig(
+				RuntimeOrigin::none(),
+				asig.try_into().unwrap(),
+				start,
+				end,
+				signature.into()
+			),
 			Error::<Test>::BeaconConfigNotSet,
 		);
 	});
@@ -106,8 +112,8 @@ fn can_submit_valid_pulses_under_the_limit() {
 		let maybe_res = SparseAccumulation::<Test>::get();
 		assert!(maybe_res.is_some());
 
-		let latest_round = LatestRound::<Test>::get();
-		assert_eq!(1002, latest_round);
+		let next_round = NextRound::<Test>::get();
+		assert_eq!(1002, next_round);
 
 		let did_update = DidUpdate::<Test>::get();
 		assert!(did_update);
@@ -143,8 +149,8 @@ fn can_submit_single_pulse() {
 			signature.into()
 		));
 
-		let latest_round = LatestRound::<Test>::get();
-		assert_eq!(1001, latest_round);
+		let next_round = NextRound::<Test>::get();
+		assert_eq!(1001, next_round);
 
 		let aggr = SparseAccumulation::<Test>::get().unwrap();
 		assert_eq!(1000, aggr.start);
@@ -159,7 +165,7 @@ fn can_fail_when_start_greater_than_end() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 		assert_ok!(Drand::set_beacon_config(RuntimeOrigin::root(), bpk.try_into().unwrap()));
-		let asig: [u8;48] = [1; 48];
+		let asig: [u8; 48] = [1; 48];
 		let start: u64 = 1001;
 		let end: u64 = 1000;
 		let encoded_data = (asig.clone().to_vec(), start, end).encode();
@@ -182,7 +188,7 @@ fn can_fail_when_sig_height_exceeds_max() {
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
 		assert_ok!(Drand::set_beacon_config(RuntimeOrigin::root(), bpk.try_into().unwrap()));
-		let asig: [u8;48] = [1; 48];
+		let asig: [u8; 48] = [1; 48];
 		let start: u64 = 1000;
 		let end: u64 = 10000;
 		let encoded_data = (asig.clone().to_vec(), start, end).encode();
@@ -247,7 +253,7 @@ fn can_submit_valid_sigs_in_sequence() {
 		assert_eq!(1002, aggr.start);
 		assert_eq!(1003, aggr.end);
 
-		let actual_latest = LatestRound::<Test>::get();
+		let actual_latest = NextRound::<Test>::get();
 		assert_eq!(1004, actual_latest);
 	});
 }
@@ -317,10 +323,10 @@ fn can_fail_to_submit_non_sequential_pulses() {
 		Drand::on_finalize(1);
 		System::set_block_number(2);
 
-	let start2: u64 = 1001;
-	let end2: u64 = 1003;
-	let encoded_data2 = (asig2.clone().to_vec(), start2, end2).encode();
-	let signature2 = alice_keypair.sign(&encoded_data2);
+		let start2: u64 = 1001;
+		let end2: u64 = 1003;
+		let encoded_data2 = (asig2.clone().to_vec(), start2, end2).encode();
+		let signature2 = alice_keypair.sign(&encoded_data2);
 
 		assert_noop!(
 			Drand::try_submit_asig(
@@ -339,7 +345,7 @@ fn can_fail_to_submit_non_sequential_pulses() {
 		assert_eq!(1000, aggr.start);
 		assert_eq!(1001, aggr.end);
 
-		let actual_latest = LatestRound::<Test>::get();
+		let actual_latest = NextRound::<Test>::get();
 		assert_eq!(1002, actual_latest);
 	});
 }
@@ -437,7 +443,7 @@ fn can_fail_with_invalid_signature() {
 		assert_eq!(1000, aggr.start);
 		assert_eq!(1001, aggr.end);
 
-		let actual_latest = LatestRound::<Test>::get();
+		let actual_latest = NextRound::<Test>::get();
 		assert_eq!(1002, actual_latest);
 	});
 }
@@ -457,7 +463,7 @@ fn first_pulse_can_be_any_round() {
 		let alice_keypair = sp_core::sr25519::Pair::from_string("//Alice", None).unwrap();
 		let signature = alice_keypair.sign(&encoded_data);
 
-		// First pulse can start at any round since latest_round == 0
+		// First pulse can start at any round since next_round == 0
 		assert_ok!(Drand::try_submit_asig(
 			RuntimeOrigin::none(),
 			asig.clone().try_into().unwrap(),
@@ -466,8 +472,8 @@ fn first_pulse_can_be_any_round() {
 			signature.into()
 		));
 
-		let latest_round = LatestRound::<Test>::get();
-		assert_eq!(1001, latest_round);
+		let next_round = NextRound::<Test>::get();
+		assert_eq!(1001, next_round);
 	});
 }
 
@@ -485,12 +491,21 @@ fn can_call_on_initialize() {
 use crate::Call;
 use codec::Encode;
 use frame_support::pallet_prelude::ValidateUnsigned;
-use sp_runtime::{MultiSignature, transaction_validity::{InvalidTransaction, TransactionPriority, TransactionSource}};
+use sp_runtime::{
+	transaction_validity::{InvalidTransaction, TransactionPriority, TransactionSource},
+	MultiSignature,
+};
 
 #[test]
 fn validate_unsigned_accepts_valid_sources_and_rejects_invalid() {
 	let (asig, _amsg, _raw) = get(vec![PULSE1000, PULSE1001]);
 	let bpk = get_beacon_pk();
+
+	let start: u64 = 1000;
+	let end: u64 = 1001;
+	let encoded_data = (asig.clone().to_vec(), start, end).encode();
+	let alice_keypair = sp_core::sr25519::Pair::from_string("//Alice", None).unwrap();
+	let signature = alice_keypair.sign(&encoded_data);
 
 	new_test_ext().execute_with(|| {
 		System::set_block_number(1);
@@ -499,15 +514,16 @@ fn validate_unsigned_accepts_valid_sources_and_rejects_invalid() {
 			bpk.clone().try_into().unwrap()
 		));
 
-		let encoded_data = Vec::new();
-
-		let alice_keypair = sp_core::sr25519::Pair::from_string("//Alice", None).unwrap();
-		let signature =MultiSignature::Sr25519(alice_keypair.sign(&encoded_data));
-
-		let call = Call::try_submit_asig { asig: asig.try_into().unwrap(), start: 1000, end: 1001 , signature: signature};
+		let call = Call::try_submit_asig {
+			asig: asig.clone().try_into().unwrap(),
+			start,
+			end,
+			signature: signature.clone().into(),
+		};
 
 		// Accept Local and InBlock sources
 		assert!(Drand::validate_unsigned(TransactionSource::Local, &call).is_ok());
+		assert!(Drand::validate_unsigned(TransactionSource::InBlock, &call).is_ok());
 
 		// Reject External source
 		let validity = Drand::validate_unsigned(TransactionSource::External, &call);
@@ -515,11 +531,19 @@ fn validate_unsigned_accepts_valid_sources_and_rejects_invalid() {
 
 		// Reject old pulses
 		// submit a valid signature
-		
+		assert_ok!(Drand::try_submit_asig(
+			RuntimeOrigin::none(),
+			asig.clone().try_into().unwrap(),
+			start,
+			end,
+			signature.clone().into()
+		));
+
 		let old_call = Call::try_submit_asig {
 			asig: asig.try_into().unwrap(),
-			start: 0, end: 1,
-			signature: signature
+			start: 0,
+			end: 1,
+			signature: signature.clone().into(),
 		};
 
 		let validity = Drand::validate_unsigned(TransactionSource::Local, &old_call);
@@ -541,8 +565,9 @@ fn validate_unsigned_has_correct_properties() {
 		let encoded_data = Vec::new();
 
 		let alice_keypair = sp_core::sr25519::Pair::from_string("//Alice", None).unwrap();
-		let signature =MultiSignature::Sr25519(alice_keypair.sign(&encoded_data));
-		let call = Call::try_submit_asig { asig: formatted.clone(), start: 1000, end: 1001, signature: signature };
+		let signature = MultiSignature::Sr25519(alice_keypair.sign(&encoded_data));
+		let call =
+			Call::try_submit_asig { asig: formatted.clone(), start: 1000, end: 1001, signature };
 
 		let validity = Drand::validate_unsigned(TransactionSource::Local, &call).unwrap();
 
@@ -567,12 +592,20 @@ fn validate_unsigned_provides_unique_tags() {
 		let encoded_data = Vec::new();
 
 		let alice_keypair = sp_core::sr25519::Pair::from_string("//Alice", None).unwrap();
-		let signature =MultiSignature::Sr25519(alice_keypair.sign(&encoded_data));
-		let call1 =
-			Call::try_submit_asig { asig: asig1.try_into().unwrap(), start: 1000, end: 1001, signature: signature.clone() };
+		let signature = MultiSignature::Sr25519(alice_keypair.sign(&encoded_data));
+		let call1 = Call::try_submit_asig {
+			asig: asig1.try_into().unwrap(),
+			start: 1000,
+			end: 1001,
+			signature: signature.clone(),
+		};
 
-		let call2 =
-			Call::try_submit_asig { asig: asig2.try_into().unwrap(), start: 1002, end: 1003, signature: signature };
+		let call2 = Call::try_submit_asig {
+			asig: asig2.try_into().unwrap(),
+			start: 1002,
+			end: 1003,
+			signature,
+		};
 
 		let validity1 = Drand::validate_unsigned(TransactionSource::Local, &call1).unwrap();
 		let validity2 = Drand::validate_unsigned(TransactionSource::Local, &call2).unwrap();
