db885fb46113bc2a7a97bf1f829a9078d605b292
Coleman Irby <ckirbyee92@gmail.com>
2025-12-18 13:51:45 -0600

    remove old types add new Precompiles type

--- chains/idn-consumer-chain/runtime/src/configs/revive.rs
+++ chains/idn-consumer-chain/runtime/src/configs/revive.rs
@@ -33,7 +33,8 @@ impl pallet_revive::Config for Runtime {
 	type Currency = Balances;
 	type RuntimeEvent = RuntimeEvent;
 	type RuntimeCall = RuntimeCall;
-	type CallFilter = AllowBalancesCall;
+	// type CallFilter = AllowBalancesCall;
+	type Precompiles = ();
 	type DepositPerItem = DepositPerItem;
 	type DepositPerByte = DepositPerByte;
 	type WeightPrice = pallet_transaction_payment::Pallet<Self>;
@@ -46,10 +47,10 @@ impl pallet_revive::Config for Runtime {
 	type InstantiateOrigin = EnsureSigned<Self::AccountId>;
 	type RuntimeHoldReason = RuntimeHoldReason;
 	type CodeHashLockupDepositPercent = CodeHashLockupDepositPercent;
-	type Xcm = pallet_xcm::Pallet<Self>;
+	// type Xcm = pallet_xcm::Pallet<Self>;
 	type ChainId = ConstU64<420_420_420>;
 	type NativeToEthRatio = ConstU32<1_000_000>; // 10^(18 - 12) Eth is 10^18, Native is 10^12.
 	type EthGasEncoder = ();
 	type FindAuthor = <Runtime as pallet_authorship::Config>::FindAuthor;
-	type ChainExtension = ();
+	// type ChainExtension = ();
 }
